25223ae775a26740665018d6e06f9e3a65f18dce
Emmanuel <65783304+EmmanuelOta@users.noreply.github.com>
2025-04-04 14:41:36 +0100

    Adding Minor UI Changes-4

--- app/page.tsx
+++ app/page.tsx
@@ -221,7 +221,7 @@ export default function Home() {
 				</section>
 
 				<section className="flex items-center justify-center p-14 w-full">
-					<div className="flex flex-col md:flex-row space-x-10 items-center justify-center p-8 md:p-0">
+					<div className="flex flex-col md:flex-row space-x-10 items-center justify-center">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
@@ -236,7 +236,7 @@ export default function Home() {
 								d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
 							/>
 						</svg>
-						<div className="flex flex-col justify-center space-y-4">
+						<div className="flex flex-col justify-center space-y-4 p-8">
 							<h3 className="text-2xl font-medium">
 								How to convert posts to images
 							</h3>
