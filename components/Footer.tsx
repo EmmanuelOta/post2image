1a9732c6cec47ce963691b22229a05538eb328f6
Emmanuel <65783304+EmmanuelOta@users.noreply.github.com>
2025-04-28 15:08:58 +0100

    Used next js link to enable prefetching

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Roboto } from "next/font/google";
+import Link from "next/link";
 
 const roboto_font = Roboto({
 	weight: "400",
@@ -20,12 +21,12 @@ const Footer = () => {
 					</p>
 				</div>
 				<div className="flex space-x-4">
-					<a
+					<Link
 						href="/terms-of-service"
 						className="text-blue-400 hover:underline"
 					>
 						Terms of Service
-					</a>
+					</Link>
 					<a
 						href="/privacy-policy"
 						className="text-blue-400 hover:underline"
@@ -37,7 +38,7 @@ const Footer = () => {
 					className="flex space-x-4"
 					title="Follow me on social media"
 				>
-					<a
+					<Link
 						href="https://x.com/coder_zi"
 						className="text-stone-800"
 						aria-label="Follow us on X"
@@ -56,8 +57,8 @@ const Footer = () => {
 								d="M17.25 3 12 10.5 6.75 3H3l7.5 9L3 21h3.75L12 13.5 17.25 21H21l-7.5-9L21 3h-3.75Z"
 							/>
 						</svg>
-					</a>
-					<a
+					</Link>
+					<Link
 						href="https://www.instagram.com/coder_zi/"
 						className="text-stone-800"
 						aria-label="Follow us on Instagram"
@@ -81,8 +82,8 @@ const Footer = () => {
 								d="M16.125 7.875h.008v.008h-.008v-.008zM12 8.625a3.375 3.375 0 1 0 0 6.75 3.375 3.375 0 0 0 0-6.75z"
 							/>
 						</svg>
-					</a>
-					<a
+					</Link>
+					<Link
 						href="https://threads.net/@coder_zi"
 						className="text-stone-800"
 						aria-label="Follow us on Threads"
@@ -92,7 +93,7 @@ const Footer = () => {
 							alt="follow coder_zi on threads"
 							width={30}
 						/>
-					</a>
+					</Link>
 				</div>
 			</div>
 		</footer>
